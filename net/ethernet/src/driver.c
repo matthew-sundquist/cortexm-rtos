4ddf0d1219274cbecf3480356cf365b01d244d74
Matthew Sundquist <matthew.sundquist@usask.ca>
2026-08-29 14:07:18 -0600

    started implementing MAC init func

--- net/ethernet/src/driver.c
+++ net/ethernet/src/driver.c
@@ -4,14 +4,51 @@
 #include "stmmac.h"
 #include "driver.h"
 
+static void tx_init();
 
-#define GOTO_ADDR(x) (*(volatile uint32_t *)(x))
+static void rx_init();
+
+static void enable_loopback();
 
 void ethernet_mac_init()
 {
-#ifdef STMMAC
+    ETH_MACCR |= ETH_MACCR_FES; // 100 Mbit/s
+    
+    ETH_MACCR |= ETH_MACCR_DM; // enable duplex mode
+}
 
-    GOTO_ADDR(ETH_MAC_BASE_ADDR + ETH_MACCR_OFFSET);
 
-#endif
+static void tx_init()
+{
+    ETH_MACCR &= ~ETH_MACCR_JD; // disable jabber, can send 16384 bytes
+    
+    // TODO: configure interframe gap
+    
+    // TODO: configure CSD
+
+    ETH_MACCR |= ETH_MACCR_RD; // come back to this, disabling retries now for simplicity
+    
+    // no need to configure Back off limit, only applies to half duplex mode
+
+    ETH_MACCR |= ETH_MACCR_TE; // enable transmitter
+}
+
+static void rx_init()
+{
+    ETH_MACCR |= ETH_MACCR_CSTF; // enable crc stripping
+
+    ETH_MACCR &= ~ETH_MACCR_WD; // disable watchdog, can recv 16384 bytes
+    
+    ETH_MACCR &= ~ETH_MACCR_IPCO; // disable ipv4 checksum offload
+
+    ETH_MACCR |= ETH_MACCR_APCS; // enable strip of crc/pad for frame with len < 1500 bytes
+
+    ETH_MACCR |= ETH_MACCR_RE; // enable receiver
+}
+
+static void enable_loopback()
+{
+    ETH_MACCR |= ETH_MACCR_LM; // enable loopback
+
+    // need to enable RX_CLK for loopback to work
 }
