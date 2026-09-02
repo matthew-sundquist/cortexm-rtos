794acec6c42e584ad39352313f43be562cf29e7b
Matthew Sundquist <matthew.sundquist@usask.ca>
2026-09-01 21:29:08 -0600

    naive implementaton of passing data into ethernet dma buf

--- net/ethernet/src/dma.c
+++ net/ethernet/src/dma.c
@@ -1,14 +1,23 @@
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "dma.h"
 #include "stmmac.h"
+
+#define NUM_RX_DESCRIPTORS 4
+#define NUM_TX_DESCRIPTORS 4
+
+#define OWN_BIT (1 << 31)
 
 static void dma_rx_init();
 
 static void dma_tx_init();
 
-static uint32_t tx_descriptors[4] __attribute__((aligned(4)));
+static bool tx_desc_put_buffer(const void *data, uint16_t len);
+
+static uint32_t tx_descriptors[NUM_TX_DESCRIPTORS];
+static uint32_t rx_descriptors[NUM_RX_DESCRIPTORS];
 
 void ethernet_dma_init()
 {
@@ -31,9 +40,29 @@ static void dma_rx_init()
 
 static void dma_tx_init()
 {
-    
 
     ETH_DMAOMR |= ETH_DMAOMR_TSF; // transmission starts when full frame in FIFO
 
     ETH_DMAOMR |= ETH_DMAOMR_ST; // start transmission
+}
+
+static bool tx_desc_put_buffer(const void *data, uint16_t len)
+{
+    if (tx_descriptors[0] & OWN_BIT) // last desc hasnt been sent yet
+    {
+        return false;
+    }
+
+    if (len > 0x1FFF) // length over max for one descriptor
+    {
+        return false;
+    }
+
+    tx_descriptors[1] = len;
+
+    tx_descriptors[2] = (uint32_t)data;
+
+    tx_descriptors[0] |= OWN_BIT;
+
+    return true;
 }
