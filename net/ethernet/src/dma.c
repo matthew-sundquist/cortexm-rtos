3e2f40318801ab5dd92eed1a78b07fe3fd630940
Matthew Sundquist <matthew.sundquist@usask.ca>
2026-09-02 08:17:35 -0600

    continued work on ethernet DMA

--- net/ethernet/src/dma.c
+++ net/ethernet/src/dma.c
@@ -4,20 +4,36 @@
 
 #include "dma.h"
 #include "stmmac.h"
+#include "netbuf.h"
 
 #define NUM_RX_DESCRIPTORS 4
 #define NUM_TX_DESCRIPTORS 4
 
-#define OWN_BIT (1 << 31)
+#define MAX_BUFFER_SIZE 0x1FFF
+
+#define OWN_BIT (1U << 31)
+#define TER_BIT (1U << 21)
+#define RER_BIT (1U << 15)
+
+typedef struct eth_dma_descriptor
+{
+    uint32_t desc[4];
+    netbuf_t *buf_owner;
+} eth_dma_descriptor_t;
 
 static void dma_rx_init();
 
 static void dma_tx_init();
 
-static bool tx_desc_put_buffer(const void *data, uint16_t len);
+static bool tx_desc_put_buffer(const void *data, size_t len);
 
-static uint32_t tx_descriptors[NUM_TX_DESCRIPTORS];
-static uint32_t rx_descriptors[NUM_RX_DESCRIPTORS];
+static size_t rx_desc_get_buffer(const void **data);
+
+static eth_dma_descriptor_t tx_descriptors[NUM_TX_DESCRIPTORS];
+static eth_dma_descriptor_t rx_descriptors[NUM_RX_DESCRIPTORS];
+
+static eth_dma_descriptor_t *rx_current;
+static eth_dma_descriptor_t *tx_current;
 
 void ethernet_dma_init()
 {
@@ -27,6 +43,9 @@ void ethernet_dma_init()
 
 static void dma_rx_init()
 {
+    rx_current = rx_descriptors[0];
+
+    rx_descriptors[NUM_RX_DESCRIPTORS - 1].desc[1] |= RER_BIT; // check back on this, could be wrong
 
     ETH_DMAOMR |= ETH_DMAOMR_DTCEFD; // disable auto dropping IP due to checksum error
     
@@ -40,29 +59,50 @@ static void dma_rx_init()
 
 static void dma_tx_init()
 {
+    tx_current = tx_descriptors[0];
 
+    tx_descriptors[NUM_TX_DESCRIPTORS - 1].desc[0] |= OWN_BIT;
+    
     ETH_DMAOMR |= ETH_DMAOMR_TSF; // transmission starts when full frame in FIFO
 
     ETH_DMAOMR |= ETH_DMAOMR_ST; // start transmission
 }
 
-static bool tx_desc_put_buffer(const void *data, uint16_t len)
+static bool tx_desc_put_buffer(const void *data, size_t len)
 {
-    if (tx_descriptors[0] & OWN_BIT) // last desc hasnt been sent yet
+    if (tx_current->desc[0] & OWN_BIT)
     {
         return false;
     }
 
-    if (len > 0x1FFF) // length over max for one descriptor
+    if (len > MAX_BUFFER_SIZE)
     {
         return false;
     }
 
-    tx_descriptors[1] = len;
+    tx_current->desc[1] = len;
+    tx_current->desc[2] = (uint32_t)data;
 
-    tx_descriptors[2] = (uint32_t)data;
+    tx_current->desc[0] |= OWN_BIT;
 
-    tx_descriptors[0] |= OWN_BIT;
+    tx_current++;
+
+    if (tx_current > tx_descriptors[NUM_TX_DESCRIPTORS - 1])
+    {
+        tx_current = tx_descriptors[0];
+    }
 
     return true;
+}
+
+static size_t rx_desc_get_buffer(const void **data)
+{
+    size_t recvd_bytes = 0;
+
+    if (rx_current->desc[0] & OWN_BIT)
+    {
+        return recvd_bytes;
+    }
+
+    if ()
 }
